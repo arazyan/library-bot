71e5f1da4bdcfbc828c55c7a126bfa57165529b4
Taunya Carli <taunyaca@student.21-school.ru>
2023-04-15 15:02:53 +0300

    Modify file

--- bot/telegram.py
+++ bot/telegram.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python
-# asynchronic library-bot
+# library-bot
 
 
-from telebot.async_telebot import AsyncTeleBot
-from config import token
-bot = AsyncTeleBot(token)
+import telebot
+from telebot import types
 
+token = '6044586998:AAFd-FQB_7n0-fk_8R8go3zCmSruejydnYI'
 
-# Handle '/start' and '/help'
+bot = telebot.TeleBot(token)
+
+book_data = {'name' : '', 'author' : '', 'year' : 0}
+
+# handle commands
 cmds = ['start', 'help', 'add', 'delete', 'list', 'find', 'borrow', 'retrieve', 'stats']
+
 @bot.message_handler(commands=cmds)
-async def send_welcome(message):
+def send_welcome(message):
     if message.text.lower() == '/start':
-        await bot.reply_to(message, """\
-    Добро пожаловать в чат бота-библиотеки!\n\nНапишите /help для вывода команд.
-    """)
+        bot.send_message(message.from_user.id, """Добро пожаловать в чат бота-библиотеки!\n\nНапишите /help для вывода команд.""")
+
     elif message.text.lower() == '/help':
-        return await help_handler(message)
+        return help_handler(message)
+    elif message.text.lower() == '/add':
+        return add_handler(message)
+    elif message.text.lower() == '/delete':
+        return delete_handler(message)
+    elif message.text.lower() == '/list':
+        return list_handler(message)
+    elif message.text.lower() == '/find':
+        return find_handler(message)
+    elif message.text.lower() == '/borrow':
+        return borrow_handler(message)
+    elif message.text.lower() == '/retrieve':
+        return retrieve_handler(message)
+    elif message.text.lower() == '/stats':
+        return stats_handler(message)
 
 
-async def help_handler(message):
-    await bot.reply_to(message, """\
+def help_handler(message):
+    bot.send_message(message.from_user.id, """\
 /start    -    начать
 /add      -    добавить книгу 
 /delete   -    удалить книгу
@@ -32,8 +50,38 @@ async def help_handler(message):
 """)
                        
 
+# /add
+def add_handler(message):
+    msg = bot.send_message(message.from_user.id, "Введите название книги:")
+    bot.register_next_step_handler(msg, add_book_name_handler)
+
+    book, author, year = '', '', ''
 
 
+def add_book_name_handler(message):
+    # добавь в базу данных объект message.text
+    book_data['name'] = message.text
+    msg = bot.send_message(message.from_user.id, "Введите автора:")
+    bot.register_next_step_handler(msg, add_author_handler)
+    
 
-import asyncio
-asyncio.run(bot.polling(none_stop=True, interval=0))
+def add_author_handler(message):
+    # добавь в базу данных объект message.text
+    book_data['author'] = message.text
+    msg = bot.send_message(message.from_user.id, "Введите год издания:")
+    bot.register_next_step_handler(msg, add_published_handler)
+
+
+def add_published_handler(message):
+    try:
+        book_data['year'] = int(message.text)
+        # генерируй айди книги
+        bot.reply_to(message, f"Книга добавлена (id)\n{repr(book_data)}")
+        # добавь в бд данные book_add_data
+    except ValueError:
+        bot.reply_to(message, "Ошибка при добавлении книги")
+        msg = bot.send_message(message.from_user.id, "Введите год издания:")
+        bot.register_next_step_handler(msg, add_published_handler)
+
+
+bot.polling(non_stop=True, interval=0)
